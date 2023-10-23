94dfd7b06572b592658730fee7572bcfc33f5bb5
hidekingerz <exthirix@gmail.com>
2023-10-24 02:01:39 +0900

    検索・ソート用関数を整理

---
+++ src/tamiya/util.ts
@@ -0,0 +1,10 @@
+import _ from "lodash";
+import { TamiyaColorData } from "./types.ts";
+
+export const findData = (searchWord: string, targetData: TamiyaColorData[]) => {
+  return targetData.filter((data: TamiyaColorData) => String(data.colorCode).includes(searchWord));
+};
+
+export const sortData = (data: TamiyaColorData[]) => {
+  return _.sortBy(data, ["colorCode"]);
+};
