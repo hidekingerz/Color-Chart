e2ec9775eb2c3a8b26fbf72768d0cfc421158a93
hidekingerz <exthirix@gmail.com>
2024-01-13 01:10:25 +0900

    TamiyaColorのテストを追加

---
+++ src/tamiya/TamiyaColor.test.tsx
@@ -0,0 +1,69 @@
+import { describe } from "vitest";
+import { screen, render } from "../lib/vitest/test-util.ts";
+import { TamiyaColor } from "./TamiyaColor.tsx";
+
+describe("TamiyaColorのテスト", () => {
+  describe('単一データのテスト', () => {
+    it("タイトルが表示されること", () => {
+      // TamiyaColorコンポーネントをダミーデータを用いてレンダリングし、ImageListItemBarに指定のタイトルが表示されることを確認する
+      const testData = [
+        {
+          colorCode: 1,
+          name: "White",
+          url: "https://www.tamiya.com/english/products/81501/img/81501_01.jpg",
+        },
+      ];
+      render(<TamiyaColor prefix="X" colorList={testData} />);
+      expect(screen.getByText("White")).toBeInTheDocument();
+    });
+  
+    it("画像が表示されること", () => {
+      // TamiyaColorコンポーネントをダミーデータを用いてレンダリングし、ImageListItemに指定の画像が表示されることを確認する
+      const testData = [
+        {
+          colorCode: 2,
+          name: "Red",
+          url: "https://www.tamiya.com/english/products/81502/img/81502_01.jpg",
+        },
+      ];
+      render(<TamiyaColor prefix="X" colorList={testData} />);
+      const imageElement = screen.getByAltText("Red");
+      expect(imageElement).toBeInTheDocument();
+      expect(imageElement).toHaveAttribute(
+        "src",
+        "https://www.tamiya.com/english/products/81502/img/81502_01.jpg"
+      );
+    });
+  });
+
+  describe('複数データのテスト', () => {
+    it("複数の画像が表示されること", () => {
+      // TamiyaColorコンポーネントをダミーデータを用いてレンダリングし、複数のImageListItemに指定の画像が表示されることを確認する
+      const testData = [
+        {
+          colorCode: 3,
+          name: "Blue",
+          url: "https://www.tamiya.com/english/products/81503/img/81503_01.jpg",
+        },
+        {
+          colorCode: 4,
+          name: "Yellow",
+          url: "https://www.tamiya.com/english/products/81504/img/81504_01.jpg",
+        },
+      ];
+      render(<TamiyaColor prefix="X" colorList={testData} />);
+      const blueImageElement = screen.getByAltText("Blue");
+      const yellowImageElement = screen.getByAltText("Yellow");
+      expect(blueImageElement).toBeInTheDocument();
+      expect(blueImageElement).toHaveAttribute(
+        "src",
+        "https://www.tamiya.com/english/products/81503/img/81503_01.jpg"
+      );
+      expect(yellowImageElement).toBeInTheDocument();
+      expect(yellowImageElement).toHaveAttribute(
+        "src",
+        "https://www.tamiya.com/english/products/81504/img/81504_01.jpg"
+      );
+    });
+  });
+});
